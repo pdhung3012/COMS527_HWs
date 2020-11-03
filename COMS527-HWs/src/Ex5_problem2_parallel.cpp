23af75cae286fefcd01d29d7993803041b553565
hungphd <hungphd@iastate.edu>
2020-11-02 20:28:06 -0600

    up

--- COMS527-HWs/src/Ex5_problem2_parallel.cpp
+++ COMS527-HWs/src/Ex5_problem2_parallel.cpp
@@ -10,7 +10,7 @@ using namespace std::chrono;
 /* Define length of dot product vectors */
 #define NT 32
 #define VECLEN 1000000000
-#define L1 2000000
+#define L1 1000000
 #define L2 10
 
 long i, j;
@@ -23,13 +23,13 @@ int main(int argc, char *argv[]) {
 	omp_set_num_threads(NT);
 	printf("Starting omp_dotprod_parallel\n");
 	auto start = high_resolution_clock::now();
-#pragma omp target
+	#pragma omp target
 	{
 		#pragma omp parallel for collapse(2) private(i,j,tid)
 		for (i = 1; i <= L1; i++) {
 			for (j = 1; j <= L2; j++) {
-			tid = omp_get_thread_num();
-				printf( "inside (i,j) (%d,%s) print thread id %d\n",i,j,tid);
+				tid = omp_get_thread_num();
+				printf( "inside (i,j) (%d,%d) print thread id %d\n",i,j,tid);
 			}
 		}
 
@@ -38,7 +38,6 @@ int main(int argc, char *argv[]) {
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(stop - start);
 
-	cout << "Time taken with parallelization: " << duration.count()
-			<< " microseconds" << endl;
+	printf( "Time taken with parallelization: %d microseconds\n", duration.count());
 
 }
