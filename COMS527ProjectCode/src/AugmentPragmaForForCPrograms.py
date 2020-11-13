620f0cbeb4d3f5c3404204007392f79cca5b6e4c
hungphd <hungphd@iastate.edu>
2020-11-12 18:52:27 -0600

    Update files

--- COMS527ProjectCode/src/AugmentPragmaForForCPrograms.py
+++ COMS527ProjectCode/src/AugmentPragmaForForCPrograms.py
@@ -6,13 +6,16 @@ import time
 import datetime
 import timeit
 from TestRunProcess import Command
-
+import clang.cindex
+from TestClangProgram import *
 
 
 fopInput='/Users/hungphan/git/COMS527/PrutorCodes/'
-fopSerialTime='/Users/hungphan/git/COMS527/SerialTimeAnalysis/'
-fopOutputAnalysis='/Users/hungphan/git/COMS527/OutputAnalysis/'
-fopTempAnalysis='/Users/hungphan/git/COMS527/TempAnalysis/'
+fopAugmented='/Users/hungphan/git/COMS527/Augment1/'
+
+# fopSerialTime='/Users/hungphan/git/COMS527/SerialTimeAnalysis/'
+# fopOutputAnalysis='/Users/hungphan/git/COMS527/OutputAnalysis/'
+# fopTempAnalysis='/Users/hungphan/git/COMS527/TempAnalysis/'
 
 
 def createDirectory(path):
@@ -23,9 +26,7 @@ def createDirectory(path):
     else:
         print("Successfully created the directory %s " % path)
 
-createDirectory(fopSerialTime)
-createDirectory(fopTempAnalysis)
-createDirectory(fopOutputAnalysis)
+createDirectory(fopAugmented)
 
 
 # #1. create array of input.txt
@@ -50,12 +51,54 @@ for currentFile in currentDirectory.glob(currentPattern):
 
 print('{}'.format(len(listFiles)))
 # Command cmd;
+
+strPragmaForTemplate='#pragma omp parallel for'
+strSetThreadFunction='omp_set_num_threads(16);'
+strIncludeOMP='#include<omp.h>'
 timeOutSecond=2
 for i in range(0,len(listFiles)):
     fileNameI=os.path.basename(listFiles[i]).replace('.c','')
-    time_fileName='time-'+fileNameI+'.txt'
-    output_fileName = 'output-'+fileNameI+'.txt'
+    fpAugmentedFile=fopAugmented+fileNameI+'.c'
     try:
+        index = clang.cindex.Index.create()
+        tu = index.parse(listFiles[i])
+        root = tu.cursor
+        index = 0
+        indexOfForLoop = 0
+        walker = Walker(listFiles[i])
+        walker.walkInForLoop(root, index, indexOfForLoop)
+        listForLoops=walker.listForLoops
+        print('Handle {} {}'.format((i+1),listFiles[i]))
+        if (len(listForLoops)>0):
+            setLineFor = set()
+            dictLines={}
+            for j in range(0, len(listForLoops)):
+                setLineFor.add(listForLoops[j].lineNumber)
+                dictLines[listForLoops[j].lineNumber]=listForLoops[j]
+            f1=open(listFiles[i],'r')
+            strF1=f1.read()
+            f1.close()
+            arrF1=strF1.split('\n')
+
+            lstNewStr=[]
+            lstNewStr.append(strIncludeOMP)
+            isAddFunction=False
+            for j in range(0,len(arrF1)):
+                if (j+1) in setLineFor:
+                    if((not isAddFunction) and (dictLines[(j+1)].funcName=='main')):
+                        lstNewStr.append(strSetThreadFunction)
+                        isAddFunction=True
+                    lstNewStr.append(strPragmaForTemplate)
+                lstNewStr.append(arrF1[j])
+
+            strAugmentedC='\n'.join(lstNewStr)
+            f1=open(fpAugmentedFile,'w')
+            f1.write(strAugmentedC)
+            f1.close()
+
+
+
+
 
 
 
